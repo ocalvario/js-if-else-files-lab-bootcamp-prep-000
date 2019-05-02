e39627f407e40645977df74706389577d88286b0
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-02 13:43:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
 
-if (name === Susan) { console.log("The name is corect")
+if (name === "Susan") { console.log("The name is corect")
 }
 
 // Don't worry about this
