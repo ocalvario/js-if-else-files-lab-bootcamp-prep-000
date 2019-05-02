b7463bfb8fb28eb28e5027b7c86fdf1bab601153
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-02 13:44:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var height = "74"
 if (name === "Susan") { console.log("The name is corect")
 }
 
+  else { console.log("Expected name to be Susan")
+  }
+
 // Don't worry about this
 module.exports = { name, height
 }
